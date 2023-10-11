7405ceb313d57189ab773eb0496df471dc1d783d
fdicera <67300276+fdicera@users.noreply.github.com>
2023-10-11 14:47:04 +0000

    Leap Year

--- class_code.rb
+++ class_code.rb
@@ -63,3 +63,24 @@ if roll1 == roll2
 else
 	pp "Sorry, you guessed #{roll1}. The die landed on #{roll2}."
 end
+
+# leap year
+years = [
+  1700,
+  1940,
+  2038
+]
+year = years.sample
+# write your program below
+
+if year % 4 == 0
+	if year % 100 == 0
+		if year % 400 == 0
+			pp "#{year} is a leap year!"
+		else
+			pp "#{year} is not a leap year."
+		end
+	else pp "#{year} is a leap year!"
+	end
+else pp "#{year} is not a leap year."
+end
