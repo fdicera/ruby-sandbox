a9ee46718578df238f5c5bc946237ff711e55226
fdicera <67300276+fdicera@users.noreply.github.com>
2023-10-11 14:58:40 +0000

    Secret Decoder

--- class_code.rb
+++ class_code.rb
@@ -97,3 +97,13 @@ pp secret
 
 pp secret.gsub("a", "1").gsub("e", "2").gsub("i", "3").gsub("o", "4").gsub("u", "5").gsub("A", "1").gsub("E", "2").gsub("I", "3").gsub("O", "4").gsub("U", "5")
 
+# Secret Decoder
+secret = [
+  "3 h1v2 1 s2cr2t t4 sh1r2",
+  "3s th3s s2c5r2 2n45gh f4r my P1SSW4RD?",
+  "w2 sh45ld b2 m4r2 cl2v2r"
+].sample
+pp secret
+# write your program below
+
+pp secret.gsub("1", "a").gsub("2", "e").gsub("3", "i").gsub("4", "o").gsub("5", "u")
